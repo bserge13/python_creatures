434a4570ac7819877b006cf0f5b1c312575eb1d8
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-08 06:07:43 -0600

    Deck class testing for functions: rank_card_at(pos), high_ranking_cards(), and percent_high_ranking(). Moving on to remove_card().

--- python_practice/m1/war_or_peace/deck.py
+++ python_practice/m1/war_or_peace/deck.py
@@ -1,3 +1,18 @@
 class Deck:
-    def __init__(self):
-        ...
+    def __init__(self, cards):
+        self.cards = cards
+
+    def rank_card_at(self, pos):
+        return self.cards[pos].rank
+
+    def high_ranking_cards(self):
+        cards = []
+        for card in self.cards:
+            if card.rank >= 11:
+                cards.append(card)
+        return cards
+    # list comprehension refactor
+        # return [card for card in self.cards if card.rank >= 11]
+
+    def percent_high_ranking(self):
+        return round(float(len(self.high_ranking_cards()) * 100) / float(len(self.cards)), 2)
