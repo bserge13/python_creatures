60182ada6eca670981dec2b9f66f177b4544de8a
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-09 19:48:41 -0600

    Addition of structure for Turn class functions type() and winner(). Suedo-code in place of logic for winner() funciton, but type() function is passing and (from what I can tell) working as expected. Logic might be able to be refactored once code testing is passing.

--- python_practice/m1/war_or_peace/turn.py
+++ python_practice/m1/war_or_peace/turn.py
@@ -4,4 +4,20 @@ class Turn:
         self.player2 = player2
         self.spoils_of_war = []
 
-# 
+    def type(self):
+        if self.player1.deck.rank_card_at(0) == self.player2.deck.rank_card_at(0):
+            if self.player1.deck.rank_of_card_at(2) == self.player2.deck.rank_of_card_at(2):
+                return 'mutually_assured_destruction'
+            return 'war'
+        else:
+            return 'basic'
+
+    def winner(self):
+        if self.type() == 'basic':
+            ...
+            # return the player whose card is higher than their opponents'
+        elif self.type() == 'war':
+            ...
+            # return the player whose .deck.rank_of_card(2) is higher than their opponents'
+        else:
+            return 'No Winner'
