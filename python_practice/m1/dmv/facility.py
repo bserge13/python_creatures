509c30d420cb540125aaffa50f898e63507ad988
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-14 06:47:33 -0600

    Setup and testing for Facility class attrs. All testing passes. Yeet.

--- python_practice/m1/dmv/facility.py
+++ python_practice/m1/dmv/facility.py
@@ -1,3 +1,6 @@
 class Facility:
-    def __init__(self):
-        ...
+    def __init__(self, facility_data):
+        self.name = facility_data['name']
+        self.address = facility_data['address']
+        self.phone = facility_data['phone']
+        self.services = []
