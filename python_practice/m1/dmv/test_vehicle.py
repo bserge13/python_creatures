a718eacb649b636b97b0c87036fb943d43060de9
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-14 06:55:42 -0600

    Testing for Vehicle class.

--- python_practice/m1/dmv/test_vehicle.py
+++ python_practice/m1/dmv/test_vehicle.py
@@ -1,2 +1,18 @@
 import pytest 
 from vehicle import Vehicle
+
+def test_vehicle_attrs():
+    cruz = Vehicle({'vin': '123456789abcdefgh', 'year': 2012, 'make': 'Chevrolet', 'model': 'Cruz', 'engine': ':ice'})
+    bolt = Vehicle({'vin': '987654321abcdefgh', 'year': 2019, 'make': 'Chevrolet', 'model': 'Bolt', 'engine': ':ev'})
+    # camaro = Vehicle({'vin': '1a2b3c4d5e6f', 'year': 1969, 'make': 'Chevrolet', 'model': 'Camaro', 'engine': ':ice'})
+    assert cruz.vin == '123456789abcdefgh'
+    assert cruz.year == 2012
+    assert cruz.make == 'Chevrolet'
+    assert cruz.model == 'Cruz'
+    assert cruz.engine == ':ice'
+
+    assert bolt.vin == '987654321abcdefgh'
+    assert bolt.year == 2019
+    assert bolt.make == 'Chevrolet'
+    assert bolt.model == 'Bolt'
+    assert bolt.engine == ':ev'
--- python_practice/m1/dmv/vehicle.py
+++ python_practice/m1/dmv/vehicle.py
@@ -1,3 +1,7 @@
 class Vehicle:
-    def __init__(self):
-        ...
+    def __init__(self, vehicle_data):
+        self.vin = vehicle_data['vin']
+        self.year = vehicle_data['year']
+        self.make = vehicle_data['make']
+        self.model = vehicle_data['model'] 
+        self.engine = vehicle_data['engine'] 
