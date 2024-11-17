97f8d2695469c2af594a22faff0faf377d9197b1
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-17 08:47:52 -0600

    Start of what I believe to be some badass api calls for dmv info and making class objects for facility and vehicle.

--- python_practice/m1/dmv/dmv_data_services.py
+++ python_practice/m1/dmv/dmv_data_services.py
@@ -7,3 +7,15 @@ class DmvDataServices:
     def wa_ev_registration(self):
         response = self.load_data('https://data.wa.gov/resource/rpr4-cgyd.json')
         return response.json()
+
+    def co_dmv_office_locations(self):
+        response = self.load_data('https://data.colorado.gov/resource/dsw3-mrn4.json')
+        return response.json()
+
+    def ny_dmv_office_locations(self):
+        response = self.load_data('https://data.ny.gov/resource/9upz-c7xg.json')
+        return response.json()
+
+    def mo_dmv_office_locations(self):
+        response = self.load_data('https://data.mo.gov/resource/835g-7keg.json') 
+        return response.json()
