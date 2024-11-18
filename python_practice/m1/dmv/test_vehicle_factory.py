154c28120189d6670f53d69864cd68858802bb6d
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-18 06:30:37 -0600

    Testing of VehicleFactory class and it's function/logic to create Vehicle objects. All testing passes. SkeeYee

--- python_practice/m1/dmv/test_vehicle_factory.py
+++ python_practice/m1/dmv/test_vehicle_factory.py
@@ -1,5 +1,21 @@
 import pytest
+from dmv_data_services import DmvDataServices 
 from vehicle_factory import VehicleFactory 
+from vehicle import Vehicle
 
 def test_create_vehicles():
-    ...
+    factory = VehicleFactory()
+    dmv = DmvDataServices()
+    wa_reg = factory.create_vehicles(dmv.wa_ev_registration())
+
+    assert isinstance(wa_reg, list)
+    assert len(wa_reg) > 0
+
+    first_wa = wa_reg[0]
+    assert isinstance(first_wa, Vehicle)
+
+    assert hasattr(first_wa, 'vin')
+    assert hasattr(first_wa, 'year')
+    assert hasattr(first_wa, 'make')
+    assert hasattr(first_wa, 'model')
+    assert hasattr(first_wa, 'engine')
--- python_practice/m1/dmv/vehicle_factory.py
+++ python_practice/m1/dmv/vehicle_factory.py
@@ -1,3 +1,15 @@
+from vehicle import Vehicle 
+
 class VehicleFactory:
-    def create_vehicles(self, location):
-        ...
+    def create_vehicles(self, service):
+        vehicles = []
+        for vehicle in service:
+            vehicle_data = {
+                'vin': vehicle['vin_1_10'],
+                'year': vehicle['model_year'],
+                'make': vehicle['make'],
+                'model': vehicle['model'],
+                'engine': ':ev'
+            }
+            vehicles.append(Vehicle(vehicle_data))
+        return vehicles
