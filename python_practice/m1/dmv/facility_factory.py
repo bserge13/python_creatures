64f9914f4dce21a1eb1f4c54895c15c47ccede6a
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-18 07:08:59 -0600

    FacilityFactory class testing for logic and function. All testing passes for specific instances of NY, CO, and MO offices with their respective keys and values. SkeeYee.

--- python_practice/m1/dmv/facility_factory.py
+++ python_practice/m1/dmv/facility_factory.py
@@ -1,3 +1,28 @@
+from facility import Facility
+
 class Facilityfactory:
     def create_facilities(self, location):
-        ...
+        new_facilities = []
+        for office in location:
+            if office['state'] == 'NY':
+                details = {
+                    'name': office['office_name'],
+                    'address': office['street_address_line_1'],
+                    'phone': office.get('public_phone_number', None),
+                }
+                new_facilities.append(Facility(details))
+            elif office['state'] == 'CO':
+                details = {
+                    'name': office['dmv_office'],
+                    'address': office['address_li'],
+                    'phone': office['phone'],
+                }
+                new_facilities.append(Facility(details))
+            elif office['state'] == 'MO':
+                details = {
+                    'name': office['name'],
+                    'address': office['address1'],
+                    'phone': office['phone'],
+                }
+                new_facilities.append(Facility(details))
+        return new_facilities
--- python_practice/m1/dmv/test_facility_factory.py
+++ python_practice/m1/dmv/test_facility_factory.py
@@ -1,5 +1,52 @@
 import pytest 
+from dmv_data_services import DmvDataServices
 from facility_factory import Facilityfactory
+from facility import Facility
 
-def test_create_facilities():
-    ...
+dmv = DmvDataServices()
+factory = Facilityfactory()
+ny_facility = factory.create_facilities(dmv.ny_dmv_office_locations())
+co_facility = factory.create_facilities(dmv.co_dmv_office_locations())
+mo_facility = factory.create_facilities(dmv.mo_dmv_office_locations())
+
+def test_create_ny_facilities():
+    assert isinstance(ny_facility, list)
+    assert len(ny_facility) > 0
+
+    first_ny = ny_facility[0]
+    assert isinstance(first_ny, Facility)
+
+    assert hasattr(first_ny, 'name')
+    assert hasattr(first_ny, 'address')
+    assert hasattr(first_ny, 'phone')
+    assert hasattr(first_ny, 'services')
+    assert hasattr(first_ny, 'registered_vehicles')
+    assert hasattr(first_ny, 'collected_fees')
+
+def test_create_co_facilities():
+    assert isinstance(co_facility, list)
+    assert len(co_facility) > 0
+
+    first_co = co_facility[0]
+    assert isinstance(first_co, Facility)
+
+    assert hasattr(first_co, 'name')
+    assert hasattr(first_co, 'address')
+    assert hasattr(first_co, 'phone')
+    assert hasattr(first_co, 'services')
+    assert hasattr(first_co, 'registered_vehicles')
+    assert hasattr(first_co, 'collected_fees')
+
+def test_create_mo_facilities():
+    assert isinstance(mo_facility, list)
+    assert len(mo_facility) > 0
+
+    first_mo = mo_facility[0]
+    assert isinstance(first_mo, Facility)
+
+    assert hasattr(first_mo, 'name')
+    assert hasattr(first_mo, 'address')
+    assert hasattr(first_mo, 'phone')
+    assert hasattr(first_mo, 'services')
+    assert hasattr(first_mo, 'registered_vehicles')
+    assert hasattr(first_mo, 'collected_fees')
