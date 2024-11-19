b274f78cfc326925bda27ee30776c78d2fea97f6
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-19 06:30:53 -0600

    Testing passes for Cell class functions fire_upon, is_fired_upon and attribute fired_upon (will look to rename attrs for ease of tracking and comprehension.

--- python_practice/m1/battleship/cell.py
+++ python_practice/m1/battleship/cell.py
@@ -2,9 +2,18 @@ class Cell:
     def __init__(self, coordinate):
         self.coordinate = coordinate
         self.ship = None
+        self.fired_upon = False
 
     def is_empty(self):
         return self.ship == None
 
     def place_ship(self, ship):
         self.ship = ship
+
+    def is_fired_upon(self):
+        return self.fired_upon
+
+    def fire_upon(self):
+        self.fired_upon = True
+        if self.ship != None:
+            self.ship.hit()
--- python_practice/m1/battleship/test_cell.py
+++ python_practice/m1/battleship/test_cell.py
@@ -16,3 +16,14 @@ def test_place_ship():
     cell.place_ship(cruiser)
     assert cell.ship == cruiser
     assert cell.is_empty() == False
+
+def test_fired_upon():
+    cell = Cell('B4')
+    cruiser = Ship('Cruiser', 3)
+
+    cell.place_ship(cruiser)
+    assert cell.is_fired_upon() == False
+
+    cell.fire_upon()
+    assert cell.ship.health == 2
+    assert cell.is_fired_upon() == True
