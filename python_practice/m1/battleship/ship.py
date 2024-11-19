4b5a2203ed3f88624236f55738218f856d4d45c2
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-19 06:01:22 -0600

    Ship class testing complete. All testing passes for attrs: name, health, length; class funciton is_sunk and hit. SkeeYee.

--- python_practice/m1/battleship/ship.py
+++ python_practice/m1/battleship/ship.py
@@ -1,3 +1,11 @@
 class Ship:
-    def __init__(self):
-        ...
+    def __init__(self, name, length):
+        self.name = name
+        self.length = length
+        self.health = length
+
+    def is_sunk(self):
+        return self.health == 0
+
+    def hit(self):
+        self.health -= 1
--- python_practice/m1/battleship/test_ship.py
+++ python_practice/m1/battleship/test_ship.py
@@ -1,5 +1,21 @@
 import pytest 
 from ship import Ship
 
-def test_ship_attrs():
-    ...
+def test_ship_class():
+    cruiser = Ship('Cruiser', 3)
+
+    assert cruiser.name == 'Cruiser'
+    assert cruiser.length == 3
+    assert cruiser.health == 3 
+    assert cruiser.is_sunk() == False
+
+    cruiser.hit()
+    assert cruiser.health == 2
+
+    cruiser.hit()
+    assert cruiser.health == 1
+    assert cruiser.is_sunk() == False
+
+    cruiser.hit()
+    assert cruiser.health == 0
+    assert cruiser.is_sunk() == True
