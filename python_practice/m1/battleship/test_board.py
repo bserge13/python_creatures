702e11ca115917db61ab271225cf103d0aecd549
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-20 06:48:51 -0600

    continued, extended testing of Board class attrs. All testing passes.

--- python_practice/m1/battleship/test_board.py
+++ python_practice/m1/battleship/test_board.py
@@ -1,5 +1,11 @@
 import pytest 
 from board import Board
 from cell import Cell
-from ship import Ship
+# from ship import Ship
 
+def test_class_attrs():
+    board = Board()
+    assert hasattr(board, 'cells')
+    assert isinstance(board.cells, dict)
+    assert isinstance(board.cells['A1'], Cell)
+    assert len(board.cells) == 16
