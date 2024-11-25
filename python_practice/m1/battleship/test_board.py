e48543ad71979cd5dd65f5abedfc491be68097f5
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-25 06:10:47 -0600

    Testing passes for SRP function, consec_coords(), logic passing for consecutive coordinates' input. SkeeYee.

--- python_practice/m1/battleship/test_board.py
+++ python_practice/m1/battleship/test_board.py
@@ -30,16 +30,16 @@ def test_valid_placement_length():
     assert board.valid_placement(cruiser, coordinates2) == True
     assert board.valid_placement(submarine, coordinates1) == True
 
-# def test_valid_placement_consecutive():
-#     coordinates1 = ['A1', 'A2', 'A4']
-#     coordinates2 = ['A1', 'C1']
-#     coordinates3 = ['A3', 'A2', 'A1']
-#     coordinates4 = ['C1', 'B1']
+def test_valid_placement_consecutive():
+    coordinates1 = ['A1', 'A2', 'A4']
+    coordinates2 = ['A1', 'C1']
+    coordinates3 = ['A3', 'A2', 'A1']
+    coordinates4 = ['C1', 'B1']
 
-#     assert board.valid_placement(cruiser, coordinates1) == False
-#     assert board.valid_placement(submarine, coordinates2) == False
-#     assert board.valid_placement(cruiser, coordinates3) == False
-#     assert board.valid_placement(submarine, coordinates4) == False
+    assert board.valid_placement(cruiser, coordinates1) == False
+    assert board.valid_placement(submarine, coordinates2) == False
+    assert board.valid_placement(cruiser, coordinates3) == False
+    assert board.valid_placement(submarine, coordinates4) == False
 
 # def test_valid_placement_diagonal():
 #     coordinates1 = ['A1', 'B2', 'C3']
