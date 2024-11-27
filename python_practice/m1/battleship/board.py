e964645c29221f674550a5612dc09efe326b4bd8
Blake Sergesketter <blakesergesketter@gmail.com>
2024-11-27 06:26:13 -0600

    Playing around with logic for consec horizontal and vertical functions. Testing not passing.

--- python_practice/m1/battleship/board.py
+++ python_practice/m1/battleship/board.py
@@ -32,9 +32,34 @@ class Board:
             if not self.valid_coordinate(coord):
                 return False
 
-        if not self.consec_vertical(coordinates) or not self.consec_horizontal(coordinates):
+        if not (self.consec_vertical(coordinates)) or not self.consec_horizontal(coordinates):
             return False
+
         return True
+
+    def consec_vertical(self, coordinates):
+        rows = [coord[0] for coord in coordinates]
+        columns = [int(coord[1]) for coord in coordinates]
+
+        if len(set(columns)) != 1:
+            return False
+        return rows == [chr(ord(rows[0]) + i) for i in range(len(rows))]
+
+        # for coord in coordinates:
+        #     if coord[0] != letter:
+        #         return False
+        #     else:
+        #         return True
+
+    def consec_horizontal(self, coordinates):
+        number = int(coordinates[0][1])
+
+        for coord in coordinates:
+            if int(coord[1]) != number:
+                return False
+            else:
+                return True
+
 
     # def consec_coords(self, coordinates):
         # rows = []
@@ -44,9 +69,3 @@ class Board:
         #     rows.append(row)
         #     columns.append(column)
     # return rows == sorted(rows) and columns == sorted(columns) and all(columns[i] == columns[i-1] + 1 for i in range(1, len(columns)))
-
-    def consec_vertical(self, coordinates):
-        ...
-
-    def consec_horizontal(self, coordinates):
-        ...
