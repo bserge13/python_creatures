25b401b351f1dc9c5521a0407a1633c62ebb29ab
Blake Sergesketter <blakesergesketter@gmail.com>
2024-12-06 06:11:31 -0600

    Potentially utilizing random library for computers random coordinates guesses and placement of ships.

--- python_practice/m1/battleship/game.py
+++ python_practice/m1/battleship/game.py
@@ -1,12 +1,15 @@
+import random 
+
 def main():
     while True:
         ui = input("Welcome to BATTLESHIP\nEnter p to play. Enter q to quit. ")
         if ui == 'q':
             print("Come back when you're ready to lose...")
         elif ui == 'p':
-            print('Looks like this is working so far')
-            break
+            return(board_setup())
 
+def board_setup():
+    print('so far so good?')
 
 if __name__ == "__main__":
     main()
