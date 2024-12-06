0a837adc3375ff4070f779ae4df072a440945813
Blake Sergesketter <blakesergesketter@gmail.com>
2024-12-06 06:04:26 -0600

    Start of runner file to put the whole game together. At the taking in user input stage of logic functionality. SkeeYee.

---
+++ python_practice/m1/battleship/game.py
@@ -0,0 +1,10 @@
+def main():
+    ui = input("Welcome to BATTLESHIP\nEnter p to play. Enter q to quit. ")
+    if ui == 'q':
+        print("Come back when you're ready to lose...")
+    elif ui == 'p':
+        print('Looks like this is working so far')
+
+
+if __name__ == "__main__":
+    main()
