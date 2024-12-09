dde57bb70ebe84d018c8cce4fb5342803f87d46e
Blake Sergesketter <blakesergesketter@gmail.com>
2024-12-09 06:31:59 -0600

    More gameplay logic added to game.py file.

--- python_practice/m1/battleship/game.py
+++ python_practice/m1/battleship/game.py
@@ -1,8 +1,20 @@
 import random 
+from board import Board
+from ship import Ship
+
+comp_board = Board()
+comp_cruiser = Ship('Cruiser', 3)
+comp_sub = Ship('Submarine', 2)
+comp_sunk_ships = 0 
+
+user_board = Board() 
+user_cruiser = Ship('Cruiser', 3)
+user_sub = Ship('Submarine', 2)
+user_sunk_ships = 0
 
 def main():
     while True:
-        ui = input("Welcome to BATTLESHIP\nEnter p to play. Enter q to quit. ")
+        ui = input('Welcome to BATTLESHIP\nEnter p to play. Enter q to quit. ')
         if ui == 'q':
             print("Come back when you're ready to lose...")
         elif ui == 'p':
@@ -21,10 +33,28 @@ def user_board_setup():
     ...
 
 def turn():
-    ...
+    print('=============COMPUTER BOARD=============')
+    print(comp_board.render())
+    print('=============PLAYER BOARD============')
+    print(user_board.render(True))
+    ui = input('Enter the coordinate for your shot:')
+    if comp_board.valid_coordinate(ui):
+        comp_board.cells[ui].fire_upon()
+        result = comp_board.cells[ui].render()
+        if result == 'X':
+            comp_sunk_ships += 1
+    else:
+        print('Please enter a valid coordinate.')
+        turn()
 
 def game_over():
-    ...
+    if user_sunk_ships == 2:
+        print("You can't defeat me you poor excuse for a Sailor!")
+    elif comp_sunk_ships == 2:
+        print('I am not a worthy Sailor, for I am but a computer!')
+    else:
+        turn()
+
 
 
 if __name__ == "__main__":
