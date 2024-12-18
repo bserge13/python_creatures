1c06640c3692bdddeb1538de1b2dceabfd330029
Blake Sergesketter <blakesergesketter@gmail.com>
2024-12-18 06:25:43 -0600

    Iteration 1 complete.

--- python_practice/m1/ICs/IC2/player.py
+++ python_practice/m1/ICs/IC2/player.py
@@ -1,3 +1,4 @@
 class Player:
-    def __init__(self):
-        ...
+    def __init__(self, player_data):
+        self.name = player_data['name']
+        self.position = player_data['position']
--- python_practice/m1/ICs/IC2/test_world_cup.py
+++ python_practice/m1/ICs/IC2/test_world_cup.py
@@ -1,2 +1,8 @@
 import pytest 
 from player import Player
+
+def test_itr1():
+    player = Player({'name': 'Luka Modric', 'position': 'midfielder'})
+    
+    assert player.name == 'Luka Modric'
+    assert player.position == 'midfielder'
