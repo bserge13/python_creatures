e8e5ab8079a170f085d72d71017370a6515c1e1c
Blake Sergesketter <blakesergesketter@gmail.com>
2024-12-19 06:16:40 -0600

    Refactored active_players_by_position() function. All tests running. Yeet.

--- python_practice/m1/ICs/IC2/worldcup.py
+++ python_practice/m1/ICs/IC2/worldcup.py
@@ -4,10 +4,15 @@ class WorldCup:
         self.teams = teams
     
     def active_players_by_position(self, position):
-        players = []
-        for team in self.teams:
-            if not team.is_eliminated():
-                for player in team.players:
-                    if player.position == position:
-                        players.append(player)
-        return players
+        # players = []
+        # for team in self.teams:
+        #     if not team.is_eliminated():
+        #         for player in team.players:
+        #             if player.position == position:
+        #                 players.append(player)
+        # return players
+        return [
+            player
+            for team in self.teams if not team.is_eliminated()
+            for player in team.players if player.position == position
+        ]
