7a970d1ad11ebe3ecdce328612cedff7b01d296e
Blake Sergesketter <blakesergesketter@gmail.com>
2024-12-19 06:13:43 -0600

    Iteration 3 complete. Code isn't pretty, but it's running...and running code is better than not-running-code. Skrrt.

--- python_practice/m1/ICs/IC2/worldcup.py
+++ python_practice/m1/ICs/IC2/worldcup.py
@@ -2,3 +2,12 @@ class WorldCup:
     def __init__(self, year, teams):
         self.year = year
         self.teams = teams
+    
+    def active_players_by_position(self, position):
+        players = []
+        for team in self.teams:
+            if not team.is_eliminated():
+                for player in team.players:
+                    if player.position == position:
+                        players.append(player)
+        return players
