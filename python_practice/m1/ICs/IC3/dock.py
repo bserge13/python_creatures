6b62a81e34e350e280a18ab0f1988a27b1e9cf2c
Blake Sergesketter <blakesergesketter@gmail.com>
2024-12-25 08:16:36 -0600

    Start of logic for charge() function in Dock class. Testing passes for happy path edge-case. Moving to stipulation of not exceeding a docks max_rental_time attr. SkeeYee.

--- python_practice/m1/ICs/IC3/dock.py
+++ python_practice/m1/ICs/IC3/dock.py
@@ -6,11 +6,12 @@ class Dock:
     
     def rent(self, boat, renter):
         self.rental_log[boat] = renter
+        boat.add_hour()
     
     def charge(self, boat):
         if boat in self.rental_log:
             renter = self.rental_log[boat]
             card = renter.credit_card_number          
-            # return {'card_number': 'renter_card_num',
-            #         'amount': 'boat pp_hr * hrs_rented'}
+            return {'card_number': card,
+                    'amount': boat.price_per_hour * boat.hours_rented}
     # if hours exceed docks max_rental_time, the extra hours are not counted/charged
--- python_practice/m1/ICs/IC3/test_boat_rental.py
+++ python_practice/m1/ICs/IC3/test_boat_rental.py
@@ -45,14 +45,14 @@ def test_itr_3():
     dock = Dock('The Rowing Dock', 3)
     
     kayak1 = Boat('kayak', 20)
-    # kayak2 = Boat('kayak', 20)
-    # board = Boat('stand_up_paddle_board', 15)
+    board = Boat('stand_up_paddle_board', 15)
     
     patrick = Renter('Patrick Star', '4242424242424242')
-    # eugene = Renter('Eugene Krabs', '1313131313131313')
+    eugene = Renter('Eugene Krabs', '1313131313131313')
     
     dock.rent(kayak1, patrick)
-    # dock.rent(kayak1, patrick)
-    # dock.rent(board, eugene)
+    dock.rent(kayak1, patrick)
+    dock.rent(board, eugene)
     
-    # assert dock.charge(kayak1) == {patrick.credit_card_number: 20}
+    assert dock.charge(kayak1) == {'card_number': patrick.credit_card_number, 'amount': 40}
+    assert dock.charge(board) == {'card_number': eugene.credit_card_number, 'amount': 15}
