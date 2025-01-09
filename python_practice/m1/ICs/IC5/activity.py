283fc226710dbfcf54e6953ff9e4f8cfa0bdf809
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-09 06:52:39 -0600

    Completion of iteration 1; all testing passes. Yeet.

--- python_practice/m1/ICs/IC5/activity.py
+++ python_practice/m1/ICs/IC5/activity.py
@@ -1,3 +1,9 @@
 class Activity:
-    def __init__(self):
-        ...
+    def __init__(self, name):
+        self.name = name
+        self.participants = {}
+        self.total_cost = 0
+    
+    def add_participant(self, participant, cost):
+        self.participants[participant] = cost
+        self.total_cost += cost
--- python_practice/m1/ICs/IC5/test_reunion.py
+++ python_practice/m1/ICs/IC5/test_reunion.py
@@ -2,4 +2,17 @@ import pytest
 from activity import Activity
 
 def test_itr_1():
-    ...
+    activity = Activity('Brunch')
+    
+    assert activity.name == 'Brunch'
+    assert activity.participants == {}
+    
+    activity.add_participant('Maria', 20)
+    assert activity.participants == {'Maria': 20}
+    
+    assert activity.total_cost == 20
+    
+    activity.add_participant('Luther', 40)
+    assert activity.participants == {'Maria': 20, 'Luther': 40}
+    
+    assert activity.total_cost == 60
