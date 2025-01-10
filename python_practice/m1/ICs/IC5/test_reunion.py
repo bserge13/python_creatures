a5a5ea2909f2e00b70b819aa6d2f835128c25c89
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-10 06:43:12 -0600

    Start of iteration 2. Added testing for the iteration, initialization of a new class, Reunion, and testing for attrs. Moving to logic for new Activity functions. Skrrt.

--- python_practice/m1/ICs/IC5/test_reunion.py
+++ python_practice/m1/ICs/IC5/test_reunion.py
@@ -1,5 +1,6 @@
 import pytest 
 from activity import Activity
+from reunion import Reunion
 
 def test_itr_1():
     activity = Activity('Brunch')
@@ -16,3 +17,19 @@ def test_itr_1():
     assert activity.participants == {'Maria': 20, 'Luther': 40}
     
     assert activity.total_cost == 60
+
+def test_itr_2():
+    activity = Activity('Brunch')
+    activity.add_participant('Maria', 20)
+    activity.add_participant('Luther', 40)
+
+    assert activity.total_cost == 60
+    assert activity.split() == 30
+    assert activity.owed() == {'Maria': 10, 'Luther': -10}
+    
+    reunion = Reunion('1406 BE')
+    
+    assert reunion.name == '1406 BE'
+    assert reunion.activities == []
+    reunion.add_activity(activity)
+    assert reunion.activities == [activity]
