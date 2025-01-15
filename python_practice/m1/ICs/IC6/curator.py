2f6484e70703052e6762a39065618a1def84ada7
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-15 07:06:48 -0600

    add_artist() function logic passing. Moving to find_artist_by_id().

--- python_practice/m1/ICs/IC6/curator.py
+++ python_practice/m1/ICs/IC6/curator.py
@@ -1,3 +1,10 @@
 class Curator:
     def __init__(self):
         self.photographs = []
+    
+    def add_artist(self, photo):
+        self.photographs.append(photo)
+    
+    def find_artist_by_id(self, id):
+        for photo in self.photographs:
+            ...
