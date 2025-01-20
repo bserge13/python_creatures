88a76dbb55507436730e7105140a80d9a833aaf7
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-20 10:33:23 -0600

    Completion of test writting for the rest of iteration 2 and Round class functions. Moving to logic writting. SkeeYee.

--- python_practice/m1/flashcards/test_flashcards.py
+++ python_practice/m1/flashcards/test_flashcards.py
@@ -2,6 +2,7 @@ import pytest
 from card import Card
 from turn import Turn
 from deck import Deck
+from round import Round
 
 def test_itr_1():
     card = Card('What is the capital of Alaska?', 'Juneau', 'Geography')
@@ -34,3 +35,28 @@ def test_itr_2():
     assert deck.cards_in_category('STEM') == [card_2, card_3]
     assert deck.cards_in_category('Geography') == [card_1]
     assert deck.cards_in_category('Pop Culture') == []
+    
+    round = Round(deck)
+    
+    assert round.deck == deck
+    assert round.turns == []
+    assert round.current_card() == card_1
+    
+    new_turn = round.take_turn('Juneau')
+    assert new_turn.is_correct() == True
+    
+    assert round.turns == [new_turn]
+    assert round.number_correct == 1
+    assert round.current_card() == card_2
+    
+    round.take_turn('Venus')
+    assert round.turns.count == 2
+    assert round.turns.last.feedback() == 'Incorrect.'
+    assert round.number_correct() == 1
+    
+    assert round.number_correct_by_category('Geography') == 1
+    assert round.number_correct_by_category('STEM') == 0
+    assert round.percent_correct() == 50.0
+    assert round.percent_correct_by_category('Geography') == 100.0
+    
+    assert round.current_card() == card_3
