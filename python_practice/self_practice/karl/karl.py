ff9d3709275b93c354521de9d16b1aaed153e815
Blake Sergesketter <blakesergesketter@gmail.com>
2025-04-23 06:17:38 -0500

    Karl class testing of attrs expectations  passing. Big Yeet.

--- python_practice/self_practice/karl/karl.py
+++ python_practice/self_practice/karl/karl.py
@@ -1,3 +1,7 @@
 class Karl:
     def __init__(self):
-        ...
+        self.name = 'Karl'
+        self.nickname = 'Big Puss'
+        self.tired = False
+        self.hungry = True 
+        self.eat_counter = 0
